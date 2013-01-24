c54352d19f1ac67fb6f7259b73701e98572269ef
liuhongjiang <andrew.lhj@gmail.com>
2013-01-24 12:50:31 +0800

    add baidu tongji

---
+++ source/javascripts/baidu-tongji.js
@@ -0,0 +1,2 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F09d1b50dde4befc675bbcde48efd7b16' type='text/javascript'%3E%3C/script%3E"));
